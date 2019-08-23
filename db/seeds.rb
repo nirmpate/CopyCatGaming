ad2a7df449c119fc0c4a2911f2ff8dd176ee8953
Nirmal Patel <nirmal.patel@sapientrazorfish.com>
2019-08-23 18:50:57 -0500

    create attack data in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,9 @@ character = Character.create(name: 'Lena the Warrior', level: 10)
 character.create_weapon(damage_min:50, damage_max: 65, strength: 20, element: 'fire', element_damage: 0.1)
 character.weapon.create_weapon_type(name: 'axe', aps: 1.2, damage_range: 'average') 
 
-# attacks = Attack.create([
-#     {name: 'Plain-Old Punch', aps_multiplier: 1, dmg_multiplier: 1, element: 'physical'},
-#     {name: 'Firemost Smite', aps_multiplier: 1.2, dmg_multiplier: 0.9, element: 'fire'},
-#     {name: 'Frosty Cleave', aps_multiplier: 0.9, dmg_multiplier: 1.2, element: 'ice'}
-#     ])
+attacks = Attack.create([
+    {name: 'Plain-Old Punch', aps_multiplier: 1, dmg_multiplier: 1, element: 'physical'},
+    {name: 'Firemost Smite', aps_multiplier: 1.2, dmg_multiplier: 0.9, element: 'fire'},
+    {name: 'Frosty Cleave', aps_multiplier: 0.9, dmg_multiplier: 1.2, element: 'ice'}
+    ])
 
